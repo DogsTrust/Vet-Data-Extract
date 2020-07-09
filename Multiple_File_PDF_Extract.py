44278949f472228d3a7ea4f3a4eef9a18a66cdc1
arwDT <64853201+arwDT@users.noreply.github.com>
2020-07-09 15:19:36 +0100

    Multiple Text Extract w/TO and Error File Handling

--- Multiple_File_PDF_Extract.py
+++ Multiple_File_PDF_Extract.py
@@ -7,6 +7,7 @@ from datetime import datetime as dt
 import os
 import func_timeout as to
 import re
+import shutil
 
 #Function to convert pdf to text file
 def convert_pdf_to_txt(path):
@@ -39,33 +40,41 @@ dt_string = now.strftime("%Y%m%d%H%M")
 #path to files
 input_path = r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Examples'
 output_path = r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Multiple Output'
+error_path = r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Multiple Output\Error Folder'
 
 filelist = os.listdir(input_path)
 
+#error checking
+error_list = []
 for i in filelist:
     path = input_path + '\{}'.format(i)
     file = os.path.splitext(i)[0]
-
     try:
         text = to.func_timeout(15, convert_pdf_to_txt, args=(path,))
         x = re.search("[a-zA-Z]", text)
-        
         if x == None:
+            error_list.append(str(path))
             print('{} Error - Empty Document'.format(file))
             pass
         else:
             # NOTE - This UnicodeEncodeError Try/Except is a bodge until I can sort out encoding
             try:
-                f = open(r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Output\{}_{}.txt'.format(file,
-                                                                                                                   dt_string),
-                         'w+')
+                f = open(r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Output\{}_{}.txt'.format(file, dt_string),'w+')
                 f.write(text)
                 f.close()
                 print('{} Success'.format(file))
             except UnicodeEncodeError:
+                error_list.append(str(path))
                 print('{} Error - Unicode Encode Error'.format(file))
     except:
+        error_list.append(str(path))
         print('{} conversion timed out'.format(file))
+
+#move files that errored out to error folder
+for file in error_list:
+    shutil.move(file, error_path)
+
+
 
 
 
