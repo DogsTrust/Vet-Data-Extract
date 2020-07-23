5729db61405eefeaf97ec84664c911f9f24f4320
arwDT <64853201+arwDT@users.noreply.github.com>
2020-07-23 15:23:25 +0100

    Multiple_File_To_PDF_Extract_Error_Handling_Update

--- Multiple_File_PDF_Extract.py
+++ Multiple_File_PDF_Extract.py
@@ -5,6 +5,9 @@ from pdfminer3.pdfpage import PDFPage
 from io import StringIO
 from datetime import datetime as dt
 import os
+import func_timeout as to
+import re
+import shutil as si
 
 #Function to convert pdf to text file
 def convert_pdf_to_txt(path):
@@ -30,25 +33,73 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
+#Function to  move files
+def file_move(file_list, destination):
+    for file in file_list:
+        si.move(file, destination)
+
+
 #date and time
 now = dt.now()
 dt_string = now.strftime("%Y%m%d%H%M")
 
-#path to files
-input_path = r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Examples'
-output_path = r'M:\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Multiple Output'
-
+#File Paths
+Drive = 'M:\\'
+input_path = os.path.join(Drive,'PDF Data Extraction','Vet Record Examples (1)','Vet Record Examples')
+output_path = os.path.join(Drive, 'PDF Data Extraction', 'Vet Record Examples (1)', 'Vet Record Text Multiple Output')
+error_path = os.path.join(Drive, 'PDF Data Extraction', 'Vet Record Examples (1)', 'Vet Record Text Multiple Output\Error Folder')
+success_path = os.path.join(Drive, 'PDF Data Extraction', 'Vet Record Examples (1)', 'Vet Record Text Multiple Output\Success Folder')
 filelist = os.listdir(input_path)
 
+
+#error checking
+error_list = []
+success_list = []
 for i in filelist:
     path = input_path + '\{}'.format(i)
     file = os.path.splitext(i)[0]
-    f = open(
-        r'\\dtdata\user$\AdamWilliams\PDF Data Extraction\Vet Record Examples (1)\Vet Record Text Multiple Output\{}_{}.txt'.format(file,dt_string),
-        'w+')
-    text = convert_pdf_to_txt(path)
-    f.write(text)
-    f.close()
+    try:
+        text = to.func_timeout(15, convert_pdf_to_txt, args=(path,))
+        x = re.search("[a-zA-Z]", text)
+        if x == None:
+            error_list.append(str(path))
+            print('{} Error - Empty Document'.format(file))
+            continue
+        else:
+            f = open(output_path + '\{}_{}.txt'.format(file, dt_string), 'w+')
+            f.write(text)
+            f.close()
+            success_list.append(str(path))
+            print('{} Success'.format(file))
+    except to.exceptions.FunctionTimedOut:
+        error_list.append(str(path))
+        print('{} conversion timed out'.format(file))
+        # NOTE - This UnicodeEncodeError Try/Except is a bodge until I can sort out encoding
+    except UnicodeEncodeError:
+        error_list.append(str(path))
+        print('{} Error - Unicode Encode Error'.format(file))
+
+#move files that errored out to error folder
+file_move(error_list, error_path)
+#move successfull file to success folder
+file_move(success_list, success_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
