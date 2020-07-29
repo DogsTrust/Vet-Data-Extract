ee7006361f2e98ba7838976eb3eb75e8401e95f9
arwDT <64853201+arwDT@users.noreply.github.com>
2020-07-29 12:39:39 +0100

    General Improvements

--- File Name Info Extract.py
+++ File Name Info Extract.py
@@ -7,45 +7,53 @@ from openpyxl import load_workbook
 #function to isolate ID numbers
 def id(lst):
     output = []
-    for ids in lst:
-        for id in ids:
-            output.append(id[1:len(i)])
+    for id in lst:
+        output.append(id[1:len(i)])
     return output
 
 #Function to isolate age
 def age_fn(lst):
     output = []
-    for ids in lst:
-        for id in ids:
-            output.append(id)
+    for id in lst:
+        output.append(id)
     return output
+
+def age_in_months(age_in):
+    ages_years = []
+    for ages in age_in:
+        yr = (re.search('\dyr', ages)).group()
+        year_num = int((re.search('\d', yr)).group())*12
+        mn = (re.search('\dm', ages)).group()
+        month_num = (int((re.search('\d', mn)).group()))
+        age_num = year_num + month_num
+        ages_years.append(age_num)
+    return ages_years
 
 #Drive and file paths
 Drive = 'M:\\'
 input_path = os.path.join(Drive,'PDF Data Extraction','Vet Record Examples (1)','Text')
 master_excel = os.path.join(Drive, 'PDF Data Extraction','Vet Record Examples (1)','Master_Record.xlsx')
-
 filelist = os.listdir(input_path)
-print(filelist)
+
 
 owner = []
 vet = []
 dog = []
 age = []
 for i in filelist:
-    owner.append(re.findall('O\d+', str(i)))
-    vet.append(re.findall('V\d+', str(i)))
-    dog.append(re.findall('D\d+', str(i)))
-    age.append(re.findall('\dyrs\s\dm', str(i)))
+    owner.append((re.search('O\d+', str(i))).group())
+    vet.append((re.search('V\d+', str(i))).group())
+    dog.append((re.search('D\d+', str(i))).group())
+    age.append((re.search('\dyrs\s\dm', str(i))).group())
 
 owner_id = id(owner)
 vet_id = id(vet)
 dog_id = id(dog)
-age = age_fn(age)
+ages = age_fn(age)
+month_age = age_in_months(ages)
+print(ages)
 
-print(owner_id, vet_id, dog_id, age)
-
-data = {'DogId':dog_id, 'OwnerId':owner_id, 'VetId':vet_id, 'Age':age}
+data = {'DogId':dog_id, 'OwnerId':owner_id, 'VetId':vet_id, 'Age (Text)':ages, 'Age (Months)':month_age}
 df= pd.DataFrame(data)
 
 #write to master doc
